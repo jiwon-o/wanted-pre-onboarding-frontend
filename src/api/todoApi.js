14a36eaceaac59a5a2f5c968923bf27de0d6d443
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-08-16 20:55:09 +0900

    ✨ feat: updateTodo API 생성 및 연동해서 체크박스 완료 여부 수정 기능 구현

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -2,12 +2,16 @@ import { accessInstance } from "./axiosInstance";
 
 export const postCreateTodo = async (data) => {
   const response = await accessInstance.post(`/todos`, data);
-  console.log(response);
   return response.data;
 };
 
 export const getGetTodo = async (data) => {
   const response = await accessInstance.get(`/todos`, data);
+  return response.data;
+};
+
+export const putUpdateTodo = async (id, data) => {
+  const response = await accessInstance.put(`/todos/${id}`, data);
   console.log(response);
   return response.data;
 };
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { postCreateTodo, getGetTodo } from "../api/todoApi";
+import { postCreateTodo, getGetTodo, putUpdateTodo } from "../api/todoApi";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
@@ -24,7 +24,6 @@ export default function Todo() {
     try {
       const data = { todo: newTodo };
       const response = await postCreateTodo(data);
-      console.log(response);
 
       const newTodoItem = {
         id: response.id,
@@ -41,10 +40,33 @@ export default function Todo() {
     }
   };
 
+  const handleTodoUpdate = async (id, updatedTodo, updatedIsCompleted) => {
+    try {
+      const response = await putUpdateTodo(id, {
+        todo: updatedTodo,
+        isCompleted: updatedIsCompleted,
+      });
+      console.log(response);
+
+      const updatedTodos = todos.map((todoItem) => {
+        if (todoItem.id === id) {
+          return {
+            ...todoItem,
+            todo: updatedTodo,
+            isCompleted: updatedIsCompleted,
+          };
+        }
+        return todoItem;
+      });
+      setTodos(updatedTodos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const getTodoList = async () => {
     try {
       const response = await getGetTodo();
-      console.log(response);
 
       setTodos(response);
     } catch (error) {
@@ -66,10 +88,20 @@ export default function Todo() {
         추가
       </button>
       <ul>
-        {todos.map((todoItem, index) => (
-          <li key={index}>
+        {todos.map((todoItem) => (
+          <li key={todoItem.id}>
             <label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                checked={todoItem.isCompleted}
+                onChange={() =>
+                  handleTodoUpdate(
+                    todoItem.id,
+                    todoItem.todo,
+                    !todoItem.isCompleted
+                  )
+                }
+              />
               <span>{todoItem.todo}</span>
             </label>
           </li>
